805055f9b7a5c8ebf74dbcbe726ceec14e95de0c
Pavel Fadeev <malachornix@gmail.com>
2015-12-22 23:24:13 +0400

    Все работает. Не хватает только обработки исключений.

--- src/FourierTransform.java
+++ src/FourierTransform.java
@@ -1,6 +1,5 @@
 import org.apache.commons.math3.complex.Complex;
 import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
@@ -8,110 +7,60 @@ import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
 import javax.swing.*;
-import java.awt.*;
 import java.io.*;
-import java.util.Scanner;
 
 public class FourierTransform {
+    private static Complex[] complex;
+    private static double[] x;
 
     public static Complex transformFunction;
-
-    public static Complex function(double x) {
-//        return new Complex(x * x);
-//        return new Complex(Math.sin(2 * x));
-        return Complex.I.multiply(4 * x).exp();
-    }
 
     public static Complex exponent(double x, double ksi) {
         return Complex.I.multiply(-2 * Math.PI * x * ksi).exp();
     }
 
-    public static Complex integrand(double x, double ksi) {
-        return function(x).multiply(exponent(x, ksi));
+    public static Complex integrand(double x, double ksi, Complex y) {
+        return y.multiply(exponent(x, ksi));
     }
 
     public static Complex calculateTransform(double step, double a, double b, double ksi) {
         Complex sum = new Complex(0, 0);
         Complex multi;
-        for (double x = a; x < b; x += step) {
-            multi = integrand(x, ksi).multiply(step);
+        /*for (int i = 0; i < x.length; i++) {
+            multi = integrand(x[i], ksi, complex[i]).multiply(step);
+            sum = new Complex(sum.getReal() + multi.getReal(), sum.getImaginary() + multi.getImaginary());
+        }*/
+        int i = 0;
+//        double h = (b - a) / 256;
+        /*for (double x = a; x < b; x += *//*h*//*step) {
+            multi = integrand(x, ksi, complex[i]).multiply(*//*h*//*step);
+            i++;
+            sum = new Complex(sum.getReal() + multi.getReal(), sum.getImaginary() + multi.getImaginary());
+        }*/
+        for (int g = 0; g < x.length; g++) {
+            multi = integrand(x[g], ksi, complex[g]).multiply(step);
             sum = new Complex(sum.getReal() + multi.getReal(), sum.getImaginary() + multi.getImaginary());
         }
         return sum;
     }
 
-    public static void showAll(double width, double a, double b, double ksi) {
-        transformFunction = calculateTransform(width, a, b, ksi);
-        System.out.println(ksi + "\t" + transformFunction.getReal() +"\t" + transformFunction.getImaginary());
-    }
-
     public static void main(String[] args) {
 
-        double precision = 0.001;
-        String functionName = "exp^(4ix)";
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Считаем преобразование Фурье от " + functionName + ".");
-        System.out.print("Введите начало промежутка интегрирования по x: ");
-        double a = scanner.nextDouble();
-        System.out.print("Введите конец промежутка интегрирования по x: ");
-        double b = scanner.nextDouble();
-        while (b <= a) {
-            System.out.println("Конец промежутка не может быть меньше начала!");
-            System.out.print("Введите конец промежутка интегрирования по x: ");
-            b = scanner.nextDouble();
-        }
-        Scanner intScanner = new Scanner(System.in);
-        System.out.print("Введите количество шагов: ");
-        int n = intScanner.nextInt();
-        while (n <= 0) {
-            System.out.println("Количество отсчетов должно быть больше 0!");
-            System.out.print("Введите количество шагов: ");
-            n = intScanner.nextInt();
-        }
-        double step = (b - a) / n;
-
-        double ksi;
-        double ksiA;
-        double ksiB;
-        double stepKsi;
-
-        System.out.print("Введите начало промежутка интегрирования по ξ: ");
-        ksiA = scanner.nextDouble();
-        System.out.print("Введите конец промежутка интегрирования по ξ: ");
-        ksiB = scanner.nextDouble();
-        while (ksiB <= ksiA) {
-            System.out.println("Конец промежутка не может быть меньше начала!");
-            System.out.print("Введите конец промежутка интегрирования по ξ: ");
-            ksiB = scanner.nextDouble();
-        }
-
-        System.out.print("Введите количество шагов: ");
-        int ksiN = intScanner.nextInt();
-        while (ksiN <= 0) {
-            System.out.println("Количество отсчетов должно быть больше 0!");
-            System.out.print("Введите количество шагов: ");
-            ksiN = intScanner.nextInt();
-        }
-
-        stepKsi = (ksiB - ksiA) / ksiN;
-
-        File fileOfTransformFunction = new File("Функция " + functionName + ".gr");
-
-        try {
-            if (!fileOfTransformFunction.exists()) {
-                fileOfTransformFunction.createNewFile();
-//                System.out.println("Файл создан");
-            }
-            else {
-//                System.out.println("Файл существует");
-            }
-        } catch (IOException e) {
-            System.out.println("Ошибка при создании файла");
-        }
+        String functionName = null;
+        int n = 0;
+        double[] ksi = null;
+        int ksiN = 0;
+        double precision;
+//        double[] x = null;
+//        double[] x = null;
+//        complex = null;
+//        Complex[] complex = null;
+        int u = 0;
+        double ksiA = 0;
+        double ksiB = 0;
 
 
-        /*JFileChooser fileChooser = new JFileChooser("function");
+        JFileChooser fileChooser = new JFileChooser();
         fileChooser.setVisible(true);
         fileChooser.setSize(800, 600);
         int ret = fileChooser.showDialog(null, "Выберите файл с функцией");
@@ -120,11 +69,61 @@ public class FourierTransform {
             try {
                 FileReader reader = new FileReader(fileChooser.getSelectedFile());
                 BufferedReader bufferedReader = new BufferedReader(reader);
-                String builder;
+                //Считали название функции
+                String builder = bufferedReader.readLine();
+                if (builder != null) {
+                    functionName = builder;
+                }
+                //Считали количество элементов ξ
+                builder = bufferedReader.readLine();
+                if (builder != null) {
+                    n = Integer.parseInt(builder);
+                    ksi = new double[n];
+                }
+                //Считали нижнюю часть ξ
+                builder = bufferedReader.readLine();
+                if (builder != null) {
+                    ksiA = Double.parseDouble(builder);
+                }
+                //Считали верхнюю часть ξ
+                builder = bufferedReader.readLine();
+                if (builder != null) {
+                    ksiB = Double.parseDouble(builder);
+                }
+                //Проверка, что верхняя часть больше нижней
+                if (ksiB <= ksiA) {
+                    System.out.println("Верхняя граница ξ меньше нижней! Поменяйте во входном файле диапазон ξ");
+                    System.exit(0);
+                }
+                //Заполняем массив ξ
+                double ksiStep = (ksiB - ksiA) / n; // Шаг
+                for (int i = 0; i < ksi.length; i++) {
+                    ksi[i] = ksiA;
+                    ksiA+=ksiStep;
+                }
+                //Создаем массив x и комлексного y
+                builder = bufferedReader.readLine();
+                if (builder != null) {
+                    int length = new Integer(builder);
+                    x = new double[length];
+                    complex = new Complex[length];
+                }
+                //Заполняем массив x и y
+                String[] splitted;
                 do {
                     builder = bufferedReader.readLine();
                     if (builder != null) {
-                        System.out.println(builder);
+                        splitted = builder.split("\\s+");
+                        if (splitted.length == 3) {
+                            x[u] = new Double(splitted[0]);
+                            complex[u] = new Complex(new Double(splitted[1]), new Double(splitted[2]));
+                            u++;
+                        } else {
+                            System.out.println("Ошибка!");
+                            System.out.println("Убедитесь, что массив значения табулированной функции состоят " +
+                                    "из трех числе в строке.");
+                            System.exit(0);
+                        }
                     }
                 } while (builder != null);
             } catch (FileNotFoundException e) {
@@ -132,31 +131,19 @@ public class FourierTransform {
             } catch (IOException e) {
                 System.out.println("Ошибка ввода/вывода!");
             }
-        }*/
-
-        XYSeriesCollection collectionOfFunction = new XYSeriesCollection();
-
-        XYSeries realSeries = new XYSeries("Re");
-        XYSeries imaginarySeries = new XYSeries("Im");
-
-        for (double i = a; i <= b ; i+=step) {
-            realSeries.add(i, function(i).getReal());
-            imaginarySeries.add(i, function(i).getImaginary());
         }
 
-        collectionOfFunction.addSeries(realSeries);
-        collectionOfFunction.addSeries(imaginarySeries);
-        JFreeChart chartOfFunction = ChartFactory.createXYLineChart("f(x) = " + functionName, "x", "y",
-                collectionOfFunction, PlotOrientation.VERTICAL, true, true, true);
+        File fileOfTransformFunction = new File("Функция " + functionName + ".gr");
 
         try {
-            ChartUtilities.saveChartAsPNG(new File("picture/График функции " + functionName + ".png"),
-                    chartOfFunction, 800, 600);
+            if (!fileOfTransformFunction.exists()) {
+                fileOfTransformFunction.createNewFile();
+            }
+            else {
+            }
         } catch (IOException e) {
-            System.out.println("Ошибка записи!");
+            System.out.println("Ошибка при создании файла");
         }
-
-
 
         XYSeriesCollection collectionOfTransformFunction = new XYSeriesCollection();
         XYSeries realSeriesOfTransformFunction = new XYSeries("Re");
@@ -169,18 +156,21 @@ public class FourierTransform {
         XYSeries seriesOfPhase = new XYSeries("Аргумент преобразования");
 
 
+        // TODO: 21.12.2015 Здесь
         FileWriter  fileWriter = null;
         try {
             fileWriter = new FileWriter(fileOfTransformFunction);
-//            fileWriter = new FileWriter(new File("exp^(4ix).gr"));
-            fileWriter.write("Преобразование Фурье от " + functionName + "\n" + (ksiN + 1) + "\n");
-            for (ksi = ksiA; ksi <= ksiB; ksi += stepKsi) {
-                transformFunction = calculateTransform(precision, a, b, ksi);
-                realSeriesOfTransformFunction.add(ksi, transformFunction.getReal());
-                imaginarySeriesOfTransformFunction.add(ksi, transformFunction.getImaginary());
-                seriesOfIntensity.add(ksi, transformFunction.abs() * transformFunction.abs());
-                seriesOfPhase.add(ksi, transformFunction.getArgument());
-                fileWriter.write(ksi + " " + transformFunction.getReal() + " " + transformFunction.getImaginary() + "\n");
+            fileWriter.write("Fourier transform " + functionName + "\n" + (ksi.length - 1) + "\n");
+            precision = (x[x.length - 1] - x[0]) / x.length;
+
+            for (double aKsi : ksi) {
+                transformFunction = calculateTransform(precision, x[0], x[x.length - 1], aKsi);
+                realSeriesOfTransformFunction.add(aKsi, transformFunction.getReal());
+                imaginarySeriesOfTransformFunction.add(aKsi, transformFunction.getImaginary());
+                seriesOfIntensity.add(aKsi, transformFunction.abs() * transformFunction.abs());
+                seriesOfPhase.add(aKsi, transformFunction.getArgument());
+                fileWriter.write(aKsi + " " + transformFunction.getReal() + " " +
+                        transformFunction.getImaginary() + "\n");
                 fileWriter.flush();
             }
 
@@ -194,6 +184,28 @@ public class FourierTransform {
                     e.printStackTrace();
                 }
             }
+        }
+
+        XYSeriesCollection collectionOfFunction = new XYSeriesCollection();
+
+        XYSeries realSeries = new XYSeries("Re");
+        XYSeries imaginarySeries = new XYSeries("Im");
+
+        for (int i = 0; i < x.length ; i++) {
+            realSeries.add(x[i], complex[i].getReal());
+            imaginarySeries.add(x[i], complex[i].getImaginary());
+        }
+
+        collectionOfFunction.addSeries(realSeries);
+        collectionOfFunction.addSeries(imaginarySeries);
+        JFreeChart chartOfFunction = ChartFactory.createXYLineChart("f(x) = " + functionName, "x", "y",
+                collectionOfFunction, PlotOrientation.VERTICAL, true, true, true);
+
+        try {
+            ChartUtilities.saveChartAsPNG(new File("picture/График функции " + functionName + ".png"),
+                    chartOfFunction, 800, 600);
+        } catch (IOException e) {
+            System.out.println("Ошибка записи!");
         }
 
         collectionOfTransformFunction.addSeries(realSeriesOfTransformFunction);
@@ -228,7 +240,5 @@ public class FourierTransform {
         } catch (IOException e) {
             System.out.println("Ошибка записи!");
         }
-
-
     }
 }
